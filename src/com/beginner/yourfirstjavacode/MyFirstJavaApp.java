a8c404243874af8ac232d8812904ec95b6aa1d8a
Gokulraj483 <107179982+Gokulraj483@users.noreply.github.com>
2022-06-09 14:29:31 +0530

    Update MyFirstJavaApp.java

--- src/com/beginner/yourfirstjavacode/MyFirstJavaApp.java
+++ src/com/beginner/yourfirstjavacode/MyFirstJavaApp.java
@@ -3,6 +3,6 @@ package com.beginner.yourfirstjavacode;
 public class MyFirstJavaApp {
 
     public static void main(String[] args) {
-        System.out.println("May the force be with you!");
+        System.out.println("Ma the force be with you!");
     }
 }
